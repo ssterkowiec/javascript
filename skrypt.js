1d95f4de30e475dca01ac60963451d6de7231360
ssterkowiec <Asteroida1!>
2020-09-27 13:24:12 +0200

    zad14

--- skrypt.js
+++ skrypt.js
@@ -1,2 +1,28 @@
-alert("hello world")
-console.log("hello world");
+// alert("hello world")
+// console.log("hello world"); //daje alert z napisem heloo world
+
+
+// let var1 = 1;
+// let var2 = 1;
+// if (var1 == var2) {
+//     console.log('var1 === var2');
+// }
+// if (var1 === var2) {
+//     console.log('var1 === var2');
+// }
+//
+// var1 = 1; // number
+// var2 = '1'//string
+// if (var1 = var2) {
+//     console.log('var1 == var2');
+// }
+// if (var1 = var2) {
+//     console.log('var1 === var2');
+// } else {
+//     console.log('var1 !== var2');
+// }
+
+
+    function alertFunction(){
+    return alert('test');
+    }
